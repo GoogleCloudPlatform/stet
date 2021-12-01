09e7ddfdc6143c733fd953e8c3f02d3075c08d48
Kevin Yap <ikeviny@google.com>
2021-12-01 14:08:39 -0800

    Add Handshake tests to conformance test suite
    This commit adds conformance tests for the Handshake endpoint, as well
    as making general improvements to the test suite (namely, printing out
    explicit error messages when certain tests fail, and ensuring
    compatibility with TLS 1.2 server implementations).
    
    PiperOrigin-RevId: 413507113
    Change-Id: I3fbb0767b8e66c4568a7d5805b6eac95d67a2695

--- cmd/conformance/main.go
+++ cmd/conformance/main.go
@@ -41,6 +41,7 @@ const (
 type ekmClient struct {
 	client client.ConfidentialEKMClient
 	shim   transportshim.ShimInterface
+	tls    *tls.Conn
 }
 
 // Initializes a new EKM client for the given version of TLS against the
@@ -53,21 +54,27 @@ func newEKMClient(keyURL string, tlsVersion int) ekmClient {
 
 	cfg := &tls.Config{
 		CipherSuites:       constants.AllowableCipherSuites,
-		MinVersion:         uint16(tlsVersion),
-		MaxVersion:         uint16(tlsVersion),
+		MinVersion:         tls.VersionTLS12,
+		MaxVersion:         tls.VersionTLS13,
 		InsecureSkipVerify: true,
 	}
 
-	tlss := tls.Client(c.shim, cfg)
+	c.tls = tls.Client(c.shim, cfg)
 
 	go func() {
-		if err := tlss.Handshake(); err != nil {
+		if err := c.tls.Handshake(); err != nil {
 			return
 		}
 	}()
 
 	return c
 }
+
+// Returns an empty byte array.
+func emptyFn([]byte) []byte { return []byte{} }
+
+// Given a byte array `b`, returns `b`.
+func identityFn(b []byte) []byte { return b }
 
 type beginSessionTest struct {
 	testName         string
@@ -103,7 +110,58 @@ func runBeginSessionTestCase(mutateTLSRecords func(r []byte) []byte) error {
 	}
 
 	return nil
+}
 
+type handshakeTest struct {
+	testName         string
+	expectErr        bool
+	mutateTLSRecords func(r []byte) []byte
+	mutateSessionKey func(s []byte) []byte
+}
+
+func runHandshakeTestCase(mutateTLSRecords, mutateSessionKey func(r []byte) []byte) error {
+	ctx := context.Background()
+
+	c := newEKMClient(*keyURI, tls.VersionTLS13)
+
+	req := &sspb.BeginSessionRequest{
+		TlsRecords: c.shim.DrainSendBuf(),
+	}
+
+	resp, err := c.client.BeginSession(ctx, req)
+	if err != nil {
+		return err
+	}
+
+	sessionContext := mutateSessionKey(resp.GetSessionContext())
+	c.shim.QueueReceiveBuf(resp.GetTlsRecords())
+
+	req2 := &sspb.HandshakeRequest{
+		SessionContext: sessionContext,
+		TlsRecords:     mutateTLSRecords(c.shim.DrainSendBuf()),
+	}
+
+	_, err = c.client.Handshake(ctx, req2)
+	if err != nil {
+		return err
+	}
+
+	// Under TLS 1.3, the TLS implementation has nothing to return here.
+	// However, attempting to call `c.tls.ConnectionState()` when the
+	// server communicates with TLS 1.2 causes the client to hang
+	// infinitely, so as a proxy, perform checks on the response records
+	// only if they are non-nil.
+	if len(resp.GetTlsRecords()) > 0 {
+		records := resp.GetTlsRecords()
+
+		// The handshake data itself is encypted, so just verify that the
+		// header for this segment of data is a handshake record.
+		if records[0] != recordHeaderHandshake {
+			return fmt.Errorf("Handshake record not received")
+		}
+	}
+
+	return nil
 }
 
 func main() {
@@ -114,7 +172,7 @@ func main() {
 		{
 			testName:         "Valid request with proper TLS Client Hello",
 			expectErr:        false,
-			mutateTLSRecords: func(r []byte) []byte { return r },
+			mutateTLSRecords: identityFn,
 		},
 		{
 			testName:  "Malformed Client Hello in request",
@@ -124,15 +182,54 @@ func main() {
 				return r
 			},
 		},
+		{
+			testName:         "No TLS records in request",
+			expectErr:        true,
+			mutateTLSRecords: emptyFn,
+		},
 	}
 
 	for _, testCase := range testCases {
 		err := runBeginSessionTestCase(testCase.mutateTLSRecords)
 		testPassed := testCase.expectErr == (err != nil)
 		if testPassed {
-			colour.Printf("^2 - %v^R\n", testCase.testName)
+			colour.Printf(" - ^2%v^R\n", testCase.testName)
 		} else {
-			colour.Printf("^1 - %v^R\n", testCase.testName)
+			colour.Printf(" - ^1%v^R (%v)\n", testCase.testName, err.Error())
+		}
+	}
+
+	// Define and run Handshake tests.
+	fmt.Println("Running Handshake tests...")
+
+	testCases2 := []handshakeTest{
+		{
+			testName:         "Valid request with proper TLS Client Handshake",
+			expectErr:        false,
+			mutateTLSRecords: identityFn,
+			mutateSessionKey: identityFn,
+		},
+		{
+			testName:         "No TLS records in request",
+			expectErr:        true,
+			mutateTLSRecords: emptyFn,
+			mutateSessionKey: identityFn,
+		},
+		{
+			testName:         "Invalid session key",
+			expectErr:        true,
+			mutateTLSRecords: identityFn,
+			mutateSessionKey: emptyFn,
+		},
+	}
+
+	for _, testCase := range testCases2 {
+		err := runHandshakeTestCase(testCase.mutateTLSRecords, testCase.mutateSessionKey)
+		testPassed := testCase.expectErr == (err != nil)
+		if testPassed {
+			colour.Printf(" - ^2%v^R\n", testCase.testName)
+		} else {
+			colour.Printf(" - ^1%v^R (%v)\n", testCase.testName, err.Error())
 		}
 	}
 }
